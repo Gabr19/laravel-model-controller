97272664f94abda5fc62a3ec3e26c39b9ea6e05c
Gabr19 <sannagabriele19@gmail.com>
2023-03-15 15:28:07 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\http\Controllers\Guest\PageController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
